40be117bdb295198042226b3b27685f782ee0b01
oopsys6 <operasystem6@naver.com>
2021-10-06 23:28:38 +0900

    도커 허브 사용해서 인스턴스에 올리는거까지 구성

---
+++ src/main/java/com/gray/cpuboundapplication/CpuBoundApplication.java
@@ -0,0 +1,13 @@
+package com.gray.cpuboundapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpuBoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpuBoundApplication.class, args);
+	}
+
+}
